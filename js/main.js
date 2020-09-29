6f8afb05f293e7a1d09fa2bd62f4266eea2f6d7f
Emily Morgan <you@example.com>
2020-09-29 01:58:28 -0400

    week 1 class work

---
+++ js/main.js
@@ -0,0 +1,11 @@
+//import the modules here
+import Person from "./modules/DataModule.js";
+import NavSystem from "./modules/TheNavSystem.js"
+
+(() => {
+    //stub * just a place for non-component specific stuff
+    //this file will be where we import our other js modules
+    console.log('loaded');
+
+    console.log(Person);
+})
