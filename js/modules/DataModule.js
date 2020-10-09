4a434543e0556599d4b69ef4fcce76c065ce7524
Emily Morgan <you@example.com>
2020-10-09 15:09:14 -0400

    adding week 1 lesson

--- js/modules/DataModule.js
+++ js/modules/DataModule.js
@@ -1,5 +1,26 @@
-export default = {
-    name: "Emily", 
-    role: "Student",
-    nikname: "EM",
+export default {
+    Justin : {
+        name: "Justin", 
+        role: "Teacher",
+        nikname: "Jus",
+    },
+
+    John: {
+        name: "John", 
+        role: "Prof",
+        nikname: "Jon",
+    },
+
+    Joe: {
+        name: "Joe", 
+        role: "Prof",
+        nikname: "Joey",
+    },
+
+    Pan: {
+        name: "Pan", 
+        role: "Code Ninja",
+        nikname: "Spider Pan",
+    },
+
 }
